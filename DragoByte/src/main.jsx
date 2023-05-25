9edd87f178848fc4c168edae62a76fa0024a90a3
Flepsz <luisfelipelfsp3@gmail.com>
2023-05-25 10:43:41 -0300

    NotFound added

--- DragoByte/src/main.jsx
+++ DragoByte/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './Home.jsx'
 import Sobre from './Sobre.jsx'
+import NotFound from "./components/NotFound/NotFound.jsx";
 import './syles/main.css'
 
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/sobre' element={<Sobre />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
