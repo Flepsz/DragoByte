3e1af6cc2513cbbf0a46dd51d88827a7d76e8c83
Flepsz <luisfelipelfsp3@gmail.com>
2023-05-25 11:21:27 -0300

    Update

--- DragoByte/src/components/NotFound/NotFound.jsx
+++ DragoByte/src/components/NotFound/NotFound.jsx
@@ -1,20 +1,21 @@
 import React from 'react'
 import Logo from './logord.png'
+import Header from '../Header/Header.jsx'
 import Footer from '../Footer/Footer.jsx'
 import "./NotFound.css";
 
 
 function NotFound() {
     return (
-        <section>
-            <article className='notFound-container'>
+        <div>
+            <Header />
+            <section className='notFound-container'>
                 <img className='notFound-img' src={Logo} alt="Prpldg" />
                 <div className="notFound-text">
-                    <h1 className='notFound-404'>404</h1>
                     <h2 className='notFound-inside'>Ops! Não encontramos essa página</h2>
                 </div>
-            </article>
-        </section>
+            </section>
+        </div>
     )
 };
 
